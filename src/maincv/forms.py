8d8d120b635bf37d34e53c629049135192ff4e52
Toufik <toufik.swar@gmail.com>
2018-06-08 15:41:14 +0200

    Changed and fixed JSON form and email message, added Loader

--- src/maincv/forms.py
+++ src/maincv/forms.py
@@ -4,8 +4,8 @@ from django import forms
 class ContactForm(forms.Form):
     full_name = forms.CharField(label="Full Name")
     email = forms.EmailField(label="Email")
-    phone = forms.CharField(label="Phone", required=False)
-    message = forms.CharField(label = "Say stuff here",
+    phone = forms.CharField(label="Phone (optional)", required=False)
+    message = forms.CharField(label = "Your message",
             widget=forms.Textarea(attrs={'rows':'8'})
     )
 
